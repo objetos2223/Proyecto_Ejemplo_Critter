83818ef3174033d64233640a7bb8281c565d7ce5
Programación Orientada a Objetos <objetos2223@gmail.com>
2016-11-10 10:37:25 -0600

    Nivel de hambre y aburrimiento

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@
  * @author cominf
  * @date 10/11/2016
  * @file main.cpp
- * @brief Esta clase representa a una mascota virtual.
+ * @brief Esta clase representa una mascota virtual. 
  */
 class Critter
 {
 private:
-    int m_hambre;
+    int m_hambre;   // nivel del hambre
+    int m_aburrido; // nivel del aburrimiento
     static int s_total;
 public:
-    void saluda();
-    Critter(int hambre = 0);
+    void habla();
+    Critter(int hambre = 0, int aburrido = 0);
     void setHambre(int hambre);
     int getHambre() const;
     static int getTotal();
@@ -30,17 +31,37 @@ int Critter::getTotal()
     return s_total;
 }
 
-Critter::Critter(int hambre)
+/**
+ * @brief Constructor del Critter
+ * @param hambre es el nivel inicial de hambre
+ * @param aburrido es el nivel inicial de aburrimiento
+ */
+Critter::Critter(int hambre, int aburrido)
+: m_hambre(hambre), m_aburrido(aburrido)
 {
     std::cout << "Ha nacido un nuevo critter!\n";
-    m_hambre = hambre;
     s_total++;
 }
 
-void Critter::saluda()
+/**
+ * @brief Muestra el estado de ánimo del critter
+ */
+void Critter::habla()
 {
-    std::cout << "Hola, soy un critter. Mi nivel de hambre es "
-                << m_hambre << std::endl;
+    int animo = m_hambre + m_aburrido;
+    std::cout << "Hola, soy un critter. Estoy ";
+    if (animo <= 5) {  
+        std::cout << "feliz\n";
+    }
+    else if (animo <= 10)   {
+        std::cout << "okay\n";
+    }
+    else if (animo <= 15)   {
+        std::cout << "frustrado\n";
+    }
+    else {
+        std::cout << "furioso\n";
+    }
 }
 
 int Critter::getHambre() const
@@ -60,17 +81,11 @@ void Critter::setHambre(int hambre)
 
 int main(int argc, char **argv)
 {
-    Critter crit1(9);
-    Critter crit2;
-    Critter crit3;
-    
-    crit1.saluda();
-    
-    crit2.setHambre(-3);
-    crit2.saluda();
-    std::cout << "El nivel de hambre es " << crit2.getHambre() << std::endl;
-    std::cout << "Total de critters creados " << Critter::getTotal() << std::endl;
-    
-    crit3.saluda();
+    Critter crit1;
+    Critter crit2(6);
+    Critter crit3(3, 8);
+    crit1.habla();
+    crit2.habla();
+    crit3.habla();
 	return 0;
 }
